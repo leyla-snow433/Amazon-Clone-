6a190faf0b657e8b5fe5d82c65d7c21d840fa2d9
leyla-snow433 <dpun14618@gmail.com>
2025-06-03 10:25:05 +0545

    project updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const products = [
     { id: 2, name: "Smart Phone", price: 250000, image: "images/phone.webp" },
     { id: 3, name: "Watch", price: 89000, image: "images/ghadi.webp" },
     { id: 4, name: "Speaker", price: 1000, image: "images/speaker.jpeg" },
-    { id: 5, name: "Macbook M3 Air", price: 349000, image: "images/laptop.jpg" }
+    { id: 5, name: "Macbook M3 Air", price: 229000, image: "images/laptop.jpg" }
    
 ];
 
